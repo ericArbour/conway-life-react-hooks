a1428c85bd9da668fabf9f9f5042d3e4cc56b8b3
Eric Arbour <arboureric@gmail.com>
2018-11-04 16:08:57 -0500

    implement game logic with passing tests

--- src/Game/helpers.js
+++ src/Game/helpers.js
@@ -1,9 +1,57 @@
 export function isAliveNext(isAlive, neighbors = 0) {
   if (neighbors < 0 || neighbors > 8) throw new Error("Invalid neighbor count");
 
-  if (isAlive && (neighbors < 2 || neighbors > 3)) return !isAlive;
+  if (isAlive && (neighbors < 2 || neighbors > 3)) return 0;
 
-  if (!isAlive && neighbors === 3) return !isAlive;
+  if (!isAlive && neighbors === 3) return 1;
 
   return isAlive;
+}
+
+export function makeGrid(rows, columns) {
+  const grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < columns; j++) {
+      row.push(0);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
+export function findCell(grid = [[]], x, y) {
+  if (x < 0 || y < 0 || y >= grid.length || x >= grid[0].length) return 0;
+  return grid[y][x];
+}
+
+export function countNeighbors(grid, x, y) {
+  return [
+    [x - 1, y - 1],
+    [x, y - 1],
+    [x + 1, y - 1],
+    [x - 1, y],
+    [x + 1, y],
+    [x - 1, y + 1],
+    [x, y + 1],
+    [x + 1, y + 1]
+  ].reduce((acc, args) => findCell(grid, ...args) + acc, 0);
+}
+
+export function computeNextGeneration(grid = [[]]) {
+  return grid.map((row, y) =>
+    row.map((cell, x) => isAliveNext(cell, countNeighbors(grid, x, y)))
+  );
+}
+
+export function updateCell(grid, x, y, value) {
+  if (x < 0 || y < 0 || y >= grid.length || x >= grid[0].length)
+    throw new Error("range out of bounds");
+
+  return grid.map((row, currentY) =>
+    row.map((cell, currentX) => {
+      if (x === currentX && y === currentY) return value;
+      return cell;
+    })
+  );
 }
--- src/Game/helpers.test.js
+++ src/Game/helpers.test.js
@@ -1,101 +1,204 @@
-import { isAliveNext } from "./helpers";
+import {
+  isAliveNext,
+  makeGrid,
+  findCell,
+  countNeighbors,
+  computeNextGeneration,
+  updateCell
+} from "./helpers";
 
 describe("isAliveNext", () => {
   test("If a cell is alive and has no neighbors, it will die", () => {
-    const isAlive = isAliveNext(true, 0);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(1, 0);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is alive and has one neighbor, it will die", () => {
-    const isAlive = isAliveNext(true, 1);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(1, 1);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is alive and has two neighbors, it will stay alive", () => {
-    const isAlive = isAliveNext(true, 2);
-    expect(isAlive).toBe(true);
+    const isAlive = isAliveNext(1, 2);
+    expect(isAlive).toBe(1);
   });
 
   test("If a cell is alive and has three neighbors, it will stay alive", () => {
-    const isAlive = isAliveNext(true, 3);
-    expect(isAlive).toBe(true);
+    const isAlive = isAliveNext(1, 3);
+    expect(isAlive).toBe(1);
   });
 
   test("If a cell is alive and has four neighbors, it will die", () => {
-    const isAlive = isAliveNext(true, 4);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(1, 4);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is alive and has five neighbors, it will die", () => {
-    const isAlive = isAliveNext(true, 5);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(1, 5);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is alive and has six neighbors, it will die", () => {
-    const isAlive = isAliveNext(true, 6);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(1, 6);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is alive and has seven neighbors, it will die", () => {
-    const isAlive = isAliveNext(true, 7);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(1, 7);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is alive and has eight neighbors, it will die", () => {
-    const isAlive = isAliveNext(true, 8);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(1, 8);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has no neighbors, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 0);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 0);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has one neighbor, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 1);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 1);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has two neighbors, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 2);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 2);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has three neighbors, it will come alive", () => {
-    const isAlive = isAliveNext(false, 3);
-    expect(isAlive).toBe(true);
+    const isAlive = isAliveNext(0, 3);
+    expect(isAlive).toBe(1);
   });
 
   test("If a cell is dead and has four neighbors, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 4);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 4);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has five neighbors, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 5);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 5);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has six neighbors, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 6);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 6);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has seven neighbors, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 7);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 7);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell is dead and has eight neighbors, it will stay dead", () => {
-    const isAlive = isAliveNext(false, 8);
-    expect(isAlive).toBe(false);
+    const isAlive = isAliveNext(0, 8);
+    expect(isAlive).toBe(0);
   });
 
   test("If a cell has more than eight neighbors, throw error", () => {
-    expect(() => isAliveNext(false, 9)).toThrowError();
+    expect(() => isAliveNext(0, 9)).toThrowError();
   });
 
   test("If a cell has less than zero neighbors, throw error", () => {
-    expect(() => isAliveNext(false, -1)).toThrowError();
+    expect(() => isAliveNext(0, -1)).toThrowError();
+  });
+});
+
+const startingGrid = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+const gridWithOneUpdate = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+const filledInGrid = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 1],
+  [0, 0, 0, 1, 1],
+  [0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0]
+];
+const nextGenGrid = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 0]
+];
+
+describe("makeGrid", () => {
+  test("can make a 5x5 grid", () => {
+    expect(makeGrid(5, 5)).toEqual(startingGrid);
+  });
+});
+
+describe("findCell", () => {
+  test("finds the expected cell", () => {
+    expect(findCell(startingGrid, 2, 1)).toBe(0);
+  });
+
+  test("finds the expected cell", () => {
+    expect(findCell(filledInGrid, 1, 1)).toBe(1);
+  });
+
+  test("returns 0 for nonexistent cell", () => {
+    expect(findCell(filledInGrid, -1, 100)).toBe(0);
+  });
+
+  test("returns 0 for nonexistent cell", () => {
+    expect(findCell(filledInGrid, 100, -1)).toBe(0);
+  });
+});
+
+describe("count neighbors", () => {
+  test("counts neighbors as expected", () => {
+    expect(countNeighbors(startingGrid, 0, 0)).toBe(0);
+  });
+  test("counts neighbors as expected", () => {
+    expect(countNeighbors(startingGrid, -1, 100)).toBe(0);
+  });
+  test("counts neighbors as expected", () => {
+    expect(countNeighbors(startingGrid, 100, -1)).toBe(0);
+  });
+  test("counts neighbors as expected", () => {
+    expect(countNeighbors(filledInGrid, 1, 0)).toBe(1);
+  });
+  test("counts neighbors as expected", () => {
+    expect(countNeighbors(filledInGrid, 3, 2)).toBe(5);
+  });
+});
+
+describe("computeNextGeneration", () => {
+  test("all 0s grid stays the same", () => {
+    expect(computeNextGeneration(startingGrid)).toEqual(startingGrid);
+  });
+
+  test("existing grid transformation obeys expected rules", () => {
+    expect(computeNextGeneration(filledInGrid)).toEqual(nextGenGrid);
+  });
+});
+
+describe("update cell", () => {
+  test("updates as expected", () => {
+    expect(updateCell(startingGrid, 1, 1, 1)).toEqual(gridWithOneUpdate);
+  });
+  test("updates as expected", () => {
+    expect(updateCell(gridWithOneUpdate, 1, 1, 0)).toEqual(startingGrid);
+  });
+  test("throws error on invalid update", () => {
+    expect(() => updateCell(startingGrid, 5, 5, 1)).toThrowError();
+  });
+  test("throws error on invalid update", () => {
+    expect(() => updateCell(startingGrid, -1, -1, 0)).toThrowError();
   });
 });
